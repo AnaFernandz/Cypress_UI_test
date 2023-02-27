eaf8c14b6d11c2526362205c69dedc6a972faeef
Ana Fernandez <apbfmm@gmail.com>
2023-02-27 15:10:18 +0100

    modificacoes teste automatizado

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,26 +2,31 @@
 
 context('Funcionalidade login', () => {
 
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+  /*   afterEach(() => {
+       cy.screenshot()
+    }); */
+    
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com') 
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.page-title').should('contain' , 'Minha conta')
-
     })
 
     it('Deve exibir mensagem de erro ao inserir usuario inválido', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+      
         cy.get('#username').type('eba@teste.com') 
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-error').should('contain' , 'Endereço de e-mail desconhecido')
     })
 
     it('Deve exibir mensagem de erro ao inserir senha inválida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+      
         cy.get('#username').type('aluno_ebac@teste.com') 
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
